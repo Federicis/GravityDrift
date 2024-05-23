5e7287f177e988a4cd37a5df88f4daca94e09518
Victor1128 <victorbadulescu7@gmail.com>
2024-05-23 16:33:41 +0300

    main menu screen seems to be working

---
+++ Assets/DataPersistence/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData {
+    public int highScore;
+    public int levelNo;
+
+    public GameData() {
+        this.highScore = 0;
+        this.levelNo = 1;
+    }
+}
