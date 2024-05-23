bfa7b49308ca1455fedb387502537ceb64e48ab1
Federicis Alexandru Ionuț <federicis.alexandru@yahoo.com>
2024-05-24 00:51:02 +0300

    Game over screen

---
+++ Assets/RocketOutOfBoudsDetector.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RocketOutOfBoudsDetector : MonoBehaviour
+{
+    string loseSceneName = "YouLoseScene";
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("RIP");
+        LoadScene();
+    }
+
+    private void LoadScene()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(loseSceneName);
+        asyncOperation.allowSceneActivation = false;
+        while (!asyncOperation.isDone)
+        {
+            if (asyncOperation.progress >= 0.9f)
+            {
+                // Allow the scene to activate
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
